374e1b5d2538c484105ae182e4735b2bd5de61d9
An Mo <ann@amurraypr.com>
2017-10-23 19:57:04 -0700

    Player Bar Fix

--- scripts/album.js
+++ scripts/album.js
@@ -37,22 +37,24 @@ var albumPicasso = {
 
      var $row = $(template);
 
+
      var clickHandler = function() {
-       var songNumber = $(this).attr('data-song-number');
+	       var songNumber = $(this).attr('data-song-number');
 
-       if (currentlyPlayingSong !== null) {
-         var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-         currentlyPlayingCell.html(currentlyPlayingSong);
-    }
-      if (currentlyPlayingSong !== songNumber) {
-
-        $(this).html(pauseButtonTemplate);
-        currentlyPlayingSong = songNumber;
-    } else if (currentlyPlayingSong === songNumber) {
-
-        $(this).html(playButtonTemplate);
-        currentlyPlayingSong = null;
-  }
+	        if (currentlyPlayingSong !== null) {
+		// Revert to song number for currently playing song because user started playing new song.
+		        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+		        currentlyPlayingCell.html(currentlyPlayingSong);
+	         }
+	        if (currentlyPlayingSong !== songNumber) {
+		// Switch from Play -> Pause button to indicate new song is playing.
+		        $(this).html(pauseButtonTemplate);
+		        currentlyPlayingSong = songNumber;
+	        } else if (currentlyPlayingSong === songNumber) {
+		// Switch from Pause -> Play button to pause currently playing song.
+		        $(this).html(playButtonTemplate);
+		        currentlyPlayingSong = null;
+	}
 };
           var onHover = function(event) {
             var songNumberCell = $(this).find('.song-item-number');
@@ -70,7 +72,7 @@ var albumPicasso = {
               songNumberCell.html(songNumber);
 }
           };
-
+    // #1
      $row.find('.song-item-number').click(clickHandler);
      // #2
      $row.hover(onHover, offHover);
@@ -108,5 +110,5 @@ var albumPicasso = {
 
 $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
-
+//deleted for loop 
 });
